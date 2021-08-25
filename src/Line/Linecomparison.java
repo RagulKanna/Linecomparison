624c1f619cc377fbc5c500300561f02a034585d1
RagulKanna <ragulragi619@gmail.com>
2021-08-26 00:31:02 +0530

    Added: equal

--- src/Line/Linecomparison.java
+++ src/Line/Linecomparison.java
@@ -12,9 +12,14 @@ public class Linecomparison {
         y1 = input.nextInt();
         x2 = input.nextInt();
         y2 = input.nextInt();
-        double Length =Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) );
-        System.out.println(Length);
-        System.out.println("Your length of the line: " + Length);
+        equals();
 
+    }
+    public static void equals()
+    {
+        if(x2==y2)
+            System.out.println("The lines are Equal");
+        else
+            System.out.println("The lines are not Equal");
     }
 }
