4eff2bd7ed7b4296dd9e6ebfc54ef2cf788a742b
RagulKanna <55151621+RagulKanna@users.noreply.github.com>
2021-08-26 13:09:41 +0530

    Update Linecomparison.java

--- src/Line/Linecomparison.java
+++ src/Line/Linecomparison.java
@@ -5,19 +5,27 @@ import java.util.Scanner;
 public class Linecomparison {
    public static int x1, x2, y1, y2;
     public static void main(String[] args) {
-        System.out.println("    Welcome to  the LineComparison");
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter values of Coordinates x1,y1,x2,y2");
-        x1 = input.nextInt();
-        y1 = input.nextInt();
-        x2 = input.nextInt();
-        y2 = input.nextInt();
-        equals();
+            System.out.println("Enter values of Coordinates for  x1,y1,x2,y2 of 1 Line ");
+            x1 = val.nextInt();
+            y1 = val.nextInt();
+            x2 = val.nextInt();
+            y2 = val.nextInt();
+            int Length1=(int)(Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) ));
+            System.out.println("Length of the Line is "+Length1);
+            System.out.println("Enter values of Coordinates for  x1,y1,x2,y2 of 1 Line ");
+            x1 = val.nextInt();
+            y1 = val.nextInt();
+            x2 = val.nextInt();
+            y2 = val.nextInt();
+            int Length2=(int)(Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) ));
+            a=Length1;
+            b=Length2;
 
+        equal();
     }
-    public static void equals()
+    public static void equal()
     {
-        if(x2==y2)
+        if(a.equals(b))
             System.out.println("The lines are Equal");
         else
             System.out.println("The lines are not Equal");
